ac60aaf973fd0641225c2afa607b9a9808971927
ANIL <ANIL@LAPTOP-PC6VSO59>
2023-01-28 14:28:02 -1200

    commit

---
+++ hybridOfActitime/src/testPackage/ActiTimeInvalidLogin.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+import genericPackage.BaseTest;
+
+public class ActiTimeInvalidLogin extends BaseTest {
+
+	
+	@Test
+	public void validTest()
+	{
+		
+	}
+}
